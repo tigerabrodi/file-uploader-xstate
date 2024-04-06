cb8990177d84138d083d3fc93327d7560abb38fe
Tiger Abrodi <tigerabrodi@gmail.com>
2024-04-06 09:18:11 +0200

    feat: implement file selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { useMachine } from '@xstate/react'
+import { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+
+import { uploadMachine } from './machines'
+
 function App() {
-  return <div>hello world</div>
+  const [state, send] = useMachine(uploadMachine)
+
+  const onDrop = useCallback(
+    (acceptedFiles: Array<File>) => {
+      send({ type: 'SELECT_FILES', files: acceptedFiles })
+    },
+    [send]
+  )
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  console.log('state', state)
+
+  return (
+    <main className="flex flex-col items-center h-screen w-screen">
+      <div className="flex flex-col items-center justify-center gap-4 mt-8">
+        <h1 className="text-4xl font-bold">Upload files</h1>
+        <div
+          {...getRootProps()}
+          className="bg-slate-700 rounded-lg p-4 text-center"
+        >
+          <input {...getInputProps()} />
+          <p className="text-slate-200">
+            {isDragActive
+              ? 'Drop the files here ...'
+              : "Drag 'n' drop some files here, or click to select files"}
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
---
+++ src/machines/uploadMachine.ts
@@ -0,0 +1,94 @@
+import { assign, fromPromise, setup } from 'xstate'
+
+import { getUploadUrl } from '../api'
+
+type TrackedFile = {
+  file: File
+  progress: number
+  state: 'pending' | 'uploading' | 'success' | 'failed'
+  size: number
+}
+
+type UploadContext = {
+  trackedFiles: Array<TrackedFile>
+  currentFileIndex: number
+  uploadId: string
+  uploadUrl: string
+}
+
+type UploadEvent = {
+  type: 'SELECT_FILES'
+  files: Array<File>
+}
+
+export const uploadMachine = setup({
+  types: {
+    context: {} as UploadContext,
+    events: {} as UploadEvent,
+  },
+  actions: {
+    setFiles: assign({
+      trackedFiles: ({ event }) =>
+        event.files.map((file) => ({
+          state: 'pending' as const,
+          progress: 0,
+          size: file.size,
+          file,
+        })),
+    }),
+    setUploadData: assign(
+      (
+        _,
+        params: {
+          url: string
+          id: string
+        }
+      ) => ({
+        uploadId: params.id,
+        uploadUrl: params.url,
+      })
+    ),
+  },
+  actors: {
+    getUploadUrl: fromPromise(getUploadUrl),
+  },
+}).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QFcAOAbA9gQwgOgEsJ0wBiAZQFEAZSgYQBUB9AMQElbyBtABgF1EoVJlgEALgUwA7QSAAeiAIwAmAJx4A7AA5Fi7Rp4AWAKyKtPLQBoQAT0QBmHpvvmAbDseOeuwwF9f1mhYuHgwYhJSUACqGDgQUQBO6KQQ0mCEUgBumADW6UFxoWDhBJExwfFJCKXZAMbYEtK8fM2ywqKNMki2iAZ4iq48g67K2qp6GvbWCgiuhk4WjvNarnquGn4BIAUhYRHRsbiJyWAJCZgJeBgNAGYXALZ4O-h7pQcVx9VZmPWdza3ddriSRdUAzVz2Yx4Iw8HgaZS6NYbax2BAAWj0TmUhlUxhxqg0igsylG-i2UkwEDgsmebREwOkshmaOUeARWi0CLUeJ04zmKMQLPWeHMOkM9nhRM59n8gUO+CIJDpHRBTMQJk0Oj0WgMJjMFkMAoQxnseFUcwhQ08sJ8su28qKJTK8uOyoZoPkDlG-VU2MUxhWSPsriNfQGQyGoy04w0kztzzwCTAuBsDEw5Tibs6atmJLNJpUxiNyhWZvM4x1Ywm8YdN2wBBIEDTAHFihmjkks6rujMfJpzTqiz10fq8I4xRKEcTSWSgA */
+  id: 'upload',
+  initial: 'idle',
+  context: {
+    trackedFiles: [],
+    currentFileIndex: 0,
+    uploadId: '',
+    uploadUrl: '',
+  },
+  states: {
+    idle: {
+      on: {
+        SELECT_FILES: {
+          actions: { type: 'setFiles' },
+          target: 'gettingUploadUrl',
+        },
+      },
+    },
+    gettingUploadUrl: {
+      invoke: {
+        src: 'getUploadUrl',
+        onDone: {
+          target: 'readyToUpload',
+          actions: {
+            type: 'setUploadData',
+            params: ({ event }) => event.output,
+          },
+        },
+        onError: {
+          target: 'failedToGetUploadUrl',
+        },
+      },
+    },
+
+    readyToUpload: {},
+
+    failedToGetUploadUrl: {},
+  },
+})
